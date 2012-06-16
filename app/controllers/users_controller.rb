d7a33fd4cd6250f0a523161eadef7e79e32efaf8
Jorge F. Contreras <windblows3@yahoo.com>
2012-06-15 19:52:17 -0500

    google analytics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to root_path
+    end
   end
 
   def update
