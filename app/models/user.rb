9ab3d709247ef2f52208007fe11ce4418827b342
Jorge F. Contreras <windblows3@yahoo.com>
2012-06-21 19:16:00 -0500

    fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-    attr_accessor :authentication_token
-	attr_accessible :email, :name, :password, :password_confirmation, :admin, :authentication_token
+	attr_accessible :email, :name, :password, :password_confirmation, :admin
 	has_secure_password
 
 	has_many :collections, dependent: :destroy
