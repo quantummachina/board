c1a60c3f4a82898a06ac5664c9be1bef5a35605d
Jorge F. Contreras <windblows3@yahoo.com>
2012-06-23 18:14:39 -0500

    uid6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:fb_access_token] = nil
+    session[:fb_user_uid] = nil
     sign_out
     redirect_to root_path
   end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,13 +2,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     
     auth = request.env['omniauth.auth']
-    session[:fb_user_uid] = auth['uid'].to_i
+    session[:fb_user_uid] = auth['uid']
     token = auth['credentials']['token']
     session[:fb_access_token] = token
 
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
-    session[:fb_access_token] = nil
     
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
