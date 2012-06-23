d75412db5758a573d7477ac6c86fda5ec7ff33dd
Jorge F. Contreras <windblows3@yahoo.com>
2012-06-23 00:28:51 -0500

    1

---
+++ db/migrate/20120623051410_change_uid_to_users.rb
@@ -0,0 +1,8 @@
+class ChangeUidToUsers < ActiveRecord::Migration
+  def up
+  	change_column :users, :uid, :integer, default: 0
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120620163748) do
+ActiveRecord::Schema.define(:version => 20120623051410) do
 
   create_table "attachments", :force => true do |t|
     t.string   "file"
@@ -66,12 +66,12 @@ ActiveRecord::Schema.define(:version => 20120620163748) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
+    t.datetime "created_at",                                        :null => false
+    t.datetime "updated_at",                                        :null => false
     t.string   "remember_token"
-    t.string   "provider",        :default => ""
-    t.string   "uid",             :default => ""
-    t.boolean  "admin",           :default => false
+    t.string   "provider",                       :default => ""
+    t.integer  "uid",             :limit => 255, :default => 0
+    t.boolean  "admin",                          :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
